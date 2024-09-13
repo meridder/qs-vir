739a5f3b3a20f9d9d2547c30a244fe3cd6ec46b4
meridder <mridder@ualberta.ca>
2024-09-13 16:39:39 -0600

    Corrected typo in jan/calibration/split_from_ms.py

--- jan/calibration/split_from_ms.py
+++ jan/calibration/split_from_ms.py
@@ -4,6 +4,6 @@ os.system('rm -rf QSVir_Jan*')
 
 # Splitting off the necessary spectral windows from the untar-ed MS file.
 # Spectral windows 0 - 15 were part of a dummy pointing and not needed for analysis.
-split(vis='22B-257.sb43147052.eb43156941.59935.54498520833/22B-257.sb43147052.eb43156941.59935.54498520833.ms',outputvis='QSVir_Jan.ms', spw='16~31,32~63,96~127', datacolumn='data')
+split(vis='22B-257.sb43242188.eb43267485.59953.67202809028/22B-257.sb43242188.eb43267485.59953.67202809028.ms',outputvis='QSVir_Jan.ms', spw='16~31,32~63,96~127', datacolumn='data')
 
 # The data are now ready for polarization calibration.
